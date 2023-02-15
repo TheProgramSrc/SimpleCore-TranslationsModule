13abc31965b724727599fed20c7398d333a9a622
Im-Fran <Im-Fran@users.noreply.github.com>
2023-02-15 12:33:40 +0000

    Deploying to gh-pages from @ TheProgramSrc/SimpleCore-TranslationsModule@7506d02dae12b65b9926994c895fd0d73d558338 🚀

---
+++ javadoc/module-search-index.js
@@ -0,0 +1,1 @@
+var moduleSearchIndex = [{"l":"TranslationsModule","url":"index.html"}]
---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+var packageSearchIndex = [{"l":"xyz.theprogramsrc.translationsmodule","url":"xyz/theprogramsrc/translationsmodule/package-summary.html"}, {"l":"All packages","url":"index.html"}]
